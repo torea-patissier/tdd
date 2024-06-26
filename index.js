ebf0b4583c745d6814ea380a7ff7bb447f4bcba8
Torea Patissier <p.torea@icloud.com>
2024-06-26 15:37:12 +0200

    failed for \n3

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ function formatSecondParam(param) {
   return param.replace(/\n/g, ',');
 }
 
-function add(a, unknownArg) {
+function add(a, b) {
   if (a === '') return 0;
-
   if (a.length > 1) {
     const split = a.split(',');
     return Number(split[0]) + Number(split[1]);
   }
   return Number(a);
+
+  if (b) {
+    return Number(a) + Number(b);
+  }
 }
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -16,9 +16,12 @@ test(' add 2.1 + 2.1 ', () => {
   expect(INDEX.add('2.1,2.1')).toBe(4.2);
 });
 
-
 test('formatSecondParam', () => {
   expect(INDEX.formatSecondParam('1\n2')).toBe('1,2');
+});
+
+test(' 1 + \n3 ', () => {
+  expect(INDEX.add('1','\n3')).toBe(4);
 });
 
 
