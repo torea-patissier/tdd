7521fed4b2c011d6adbe121eb0d7023822b2557f
Torea Patissier <p.torea@icloud.com>
2024-06-26 14:52:26 +0200

    refacto

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function add(a) {
-  if (a === '') {
-    return 0;
-  }
+  if (a === '') return 0;
+
   if (a.length > 1) {
     const split = a.split(',');
     return Number(split[0]) + Number(split[1]);
   }
+
   return Number(a);
 }
 
