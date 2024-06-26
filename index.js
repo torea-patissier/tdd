7d3f9cb50896d5d699df3ffc23e11e8925b724b1
Torea Patissier <p.torea@icloud.com>
2024-06-26 15:11:49 +0200

    format second param

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-function add(a) {
+function formatSecondParam(param) {
+  return param.replace(/\n/g, ',');
+}
+
+function add(a, unknownArg) {
   if (a === '') return 0;
 
   if (a.length > 1) {
     const split = a.split(',');
     return Number(split[0]) + Number(split[1]);
   }
-
   return Number(a);
 }
 
 module.exports = {
-  add
+  add,
+  formatSecondParam
 };
--- index.test.js
+++ index.test.js
@@ -8,6 +8,17 @@ test(' return 1 ', () => {
   expect(INDEX.add('1')).toBe(1);
 });
 
-test(' return 3 ', () => {
+test(' add 1 + 2 ', () => {
     expect(INDEX.add('1,2')).toBe(3);
 });
+
+test(' add 2.1 + 2.1 ', () => {
+  expect(INDEX.add('2.1,2.1')).toBe(4.2);
+});
+
+
+test('formatSecondParam', () => {
+  expect(INDEX.formatSecondParam('1\n2')).toBe('1,2');
+});
+
+
